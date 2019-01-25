8eefb134a206c8ac2153cb9d99207b68617a7b18
hyeyoung0112 <hyeyoung0112@gmail.com>
2019-01-25 13:59:22 +0900

    리스트뷰 activity 만듦 + progress bar ing

--- app/src/main/java/com/madcamp/lionmusic/MainActivity.java
+++ app/src/main/java/com/madcamp/lionmusic/MainActivity.java
@@ -3,18 +3,13 @@ package com.madcamp.lionmusic;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
-import android.provider.Settings;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -22,7 +17,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/madcamp/lionmusic/SongsActivity.java
@@ -0,0 +1,13 @@
+package com.madcamp.lionmusic;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SongsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_songs);
+    }
+}
