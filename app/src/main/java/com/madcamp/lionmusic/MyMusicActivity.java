a0c9281de9e620facf584f123b61249dbaa0c17b
hyeyoung0112 <hyeyoung0112@gmail.com>
2019-01-26 16:27:17 +0900

    drawable push kakaotalk 설정

---
+++ app/src/main/java/com/madcamp/lionmusic/MyMusicActivity.java
@@ -0,0 +1,11 @@
+package com.madcamp.lionmusic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyMusicActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
