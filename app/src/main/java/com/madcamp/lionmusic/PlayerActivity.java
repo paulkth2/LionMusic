06523d6d712a77436ccf2bdca2e87366bf4e02bd
Taehyung Kim <paulkth2@kaist.ac.kr>
2019-01-26 15:11:23 +0900

    Merge branch 'master' of https://github.com/paulkth2/LionMusic
    # Conflicts:
    #	.idea/vcs.xml
    #	app/build.gradle
    #	app/src/main/java/com/madcamp/lionmusic/MainActivity.java
    #	app/src/main/java/com/madcamp/lionmusic/SongsActivity.java
    #	app/src/main/res/layout/activity_songs.xml
    #	app/src/main/res/layout/song_item.xml

--- app/src/main/java/com/madcamp/lionmusic/PlayerActivity.java
+++ app/src/main/java/com/madcamp/lionmusic/PlayerActivity.java
@@ -3,14 +3,35 @@ package com.madcamp.lionmusic;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class PlayerActivity extends AppCompatActivity {
+    private TextView titleText;
+    private SeekBar seekBar;
+    private ToggleButton playButton;
+    private ImageButton rewindButton;
+    private ImageButton forwardButton;
+    private TextView timeText;
 
+    private String songTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.music_player);
+
+        songTitle = getIntent().getStringExtra("title");
+
+        //initialize views
+        titleText = (TextView)findViewById(R.id.playerSongTitle);
+        timeText = (TextView)findViewById(R.id.timeLeft);
+        playButton = (ToggleButton)findViewById(R.id.playToggle);
+        rewindButton = (ImageButton)findViewById(R.id.rewindBackButton);
+        forwardButton = (ImageButton)findViewById(R.id.rewindForwardButton);
+
 
 
     }
