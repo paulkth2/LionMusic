11026aa21b74278612611d3dab79541e2a77e6bf
Taehyung Kim <paulkth2@kaist.ac.kr>
2019-01-28 16:31:08 +0900

    Merge branch 'master' of https://github.com/paulkth2/LionMusic
    # Conflicts:
    #	app/src/main/java/com/madcamp/lionmusic/SearchActivity.java

--- app/src/main/java/com/madcamp/lionmusic/LoginActivity.java
+++ app/src/main/java/com/madcamp/lionmusic/LoginActivity.java
@@ -1,18 +1,41 @@
 package com.madcamp.lionmusic;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ImageButton closeButton;
 
+    private EditText email;
+    private EditText pass;
+    private Button login;
+    private Button register;
+
+    private FirebaseAuth firebaseAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         closeButton = findViewById(R.id.loginCloseButton);
 
@@ -22,6 +45,87 @@ public class LoginActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        email = (EditText) findViewById(R.id.EmailInput);
+        pass = (EditText) findViewById(R.id.PWInput);
+        login = (Button) findViewById(R.id.loginButton);
+        register = (Button) findViewById(R.id.registerButton);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailtxt = email.getText().toString();
+                String passwordtxt = pass.getText().toString();
+
+                if(TextUtils.isEmpty(emailtxt)){
+                    Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(passwordtxt)){
+                    Toast.makeText(getApplicationContext(),"Please fill in the required fields",Toast.LENGTH_SHORT).show();
+                }
+
+                if(passwordtxt.length()<6){
+                    Toast.makeText(getApplicationContext(),"Password must be at least 6 characters",Toast.LENGTH_SHORT).show();
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(emailtxt, passwordtxt)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(getApplicationContext(),"E-mail or password is wrong",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "You must fill out all the fields", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    firebaseAuth.signInWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    Toast.makeText(LoginActivity.this, "User signed in", Toast.LENGTH_LONG).show();
+                                    //Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    //startActivity(intent);
+                                    finish();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(LoginActivity.this, "Sign in failed due to "+e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+
+            }
+        });
+
+    }
+
+    private boolean isEmpty(){
+        if(TextUtils.isEmpty(email.getText().toString())){
+            email.setError("Required to write an Email address!");
+            return true;
+        }
+        if (TextUtils.isEmpty(pass.getText().toString())){
+            pass.setError("Required to write a password!");
+            return true;
+        }
+        return false;
     }
 
     @Override
