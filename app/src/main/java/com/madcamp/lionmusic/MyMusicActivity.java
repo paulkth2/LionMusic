3c07d4bcaa1bb00da7be40d6ff6bfe3e941d8194
hyeyoung0112 <hyeyoung0112@gmail.com>
2019-01-28 14:44:04 +0900

    My page Activity & xml

--- app/src/main/java/com/madcamp/lionmusic/MyMusicActivity.java
+++ app/src/main/java/com/madcamp/lionmusic/MyMusicActivity.java
@@ -1,55 +1,12 @@
 package com.madcamp.lionmusic;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
-import com.kakao.auth.ISessionCallback;
-import com.kakao.auth.Session;
-import com.kakao.util.exception.KakaoException;
-import com.kakao.util.helper.log.Logger;
-
-public class MyMusicActivity extends Activity {
-
-    private SessionCallback callback;
-
+public class MyMusicActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mymusic);
-
-        callback = new SessionCallback();
-        Session.getCurrentSession().addCallback(callback);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
-
-            return;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Session.getCurrentSession().removeCallback(callback);
-    }
-
-    private class SessionCallback implements ISessionCallback {
-        @Override
-        public void onSessionOpened() {
-            Toast.makeText(getApplicationContext(), "로그인에 실패했습니다.", Toast.LENGTH_LONG).show();
-            onBackPressed();
-        }
-
-        @Override
-        public void onSessionOpenFailed(KakaoException exception) {
-            if(exception != null) {
-                Logger.e(exception);
-            }
-        }
     }
 }
